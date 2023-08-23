99433074d838cc94ec078a496f5c2aa557c35515
Dharminder1987 <131650083+Dharminder1987@users.noreply.github.com>
2023-08-23 11:34:08 +0530

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  important: true,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
